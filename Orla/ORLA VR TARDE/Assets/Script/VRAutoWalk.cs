73bf10bb84f2e0c39e056301cf1a731badf23ec4
michaelsnoww <mich.mdn21@gmail.com>
2018-03-22 18:09:22 -0400

    testes

---
+++ Orla/ORLA VR TARDE/Assets/Script/VRAutoWalk.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(CharacterController))]
+public class VRAutoWalk : MonoBehaviour
+{
+    public float speed = 3.0F;
+    public bool moveForward;
+    private CharacterController controller;
+    private Transform vrHead;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        vrHead = Camera.main.transform;
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            moveForward = !moveForward;
+        }
+
+        if (moveForward)
+        {
+            Vector3 forward = vrHead.TransformDirection(Vector3.forward);
+            controller.SimpleMove(forward * speed);
+        }
+    }
+}
